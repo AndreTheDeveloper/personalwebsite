83f66b07322c0cb54eecfdc08c29c29e3a0247ca
AndreTheDeveloper <andre.colaris98@live.ca>
2021-08-03 21:03:35 -0600

    Got a parralax effect on background. Started working on 'WHO I AM' section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Title from './components/Title'
+import Background from './components/Background.js'
 
 function App() {
   return (
     <div>
       <Title />
+        <Background />
     </div>
   );
 }
