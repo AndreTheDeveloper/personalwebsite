ff74f08268587889d9ae3faed699ca4572804cef
AndreTheDeveloper <andre.colaris98@live.ca>
2021-08-05 19:20:02 -0600

    Added fad-in animation when scrolling

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import Title from './components/Title'
 import Background from './components/Background.js'
 import Projects from './components/Projects.js'
 import Skills from './components/SkillSet.js'
+import Aos from 'aos';
+import 'aos/dist/aos.css'
+import {useEffect} from "react";
 
 function App() {
+  useEffect(() => {
+    Aos.init({duration: 2000});
+  }, [])
   return (
     <div>
       <Title />
+      <div data-aos='fade-up'>
         <Background />
+      </div>
+      <div data-aos='fade-up'>
         <Projects />
+      </div>
+      <div data-aos='fade-up'>
         <Skills />
+      </div>
     </div>
   );
 }
