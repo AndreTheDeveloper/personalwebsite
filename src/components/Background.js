c1737b7965a4eb19ecffa08aebbeff1b2292d337
AndreTheDeveloper <andre.colaris98@live.ca>
2022-01-05 20:49:20 -0700

    added bootcamp to render

--- src/components/Background.js
+++ src/components/Background.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import '../css/backgroundCSS.css'
 import Sait from './SaitBackground'
 import Job from './GlassmastersBackground'
+import Bootcamp from './HarvestBuildersBackground'
 
 class Background extends Component {
     render() {
@@ -10,6 +11,7 @@ class Background extends Component {
                 <div className='backgroundTitle'>
                     ---My Experiences---
                 </div>
+                <Bootcamp />
                 <Sait/>
                 <Job />
             </div>
